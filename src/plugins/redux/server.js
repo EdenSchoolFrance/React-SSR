cf9a7f62ac8a29822a7c1ffff95d2f44054ccf54
Jérémy JOURDIN <jeremy.jourdin@free.fr>
2018-07-17 17:04:05 +0200

    Implements caching

--- src/plugins/redux/server.js
+++ src/plugins/redux/server.js
@@ -1,4 +1,4 @@
-export default (server, { initVar = 'window.__PRELOADED_STATE__' }) => {
+export default (server, { initVar = 'window.__PRELOADED_STATE__' } = {}) => {
 	server.on('prepare', ({ document, context: { store } }) => {
 		const serializedStore = JSON.stringify(store.getState()).replace(/</g, '\\u003c');
 		const script = document.createElement('script');
