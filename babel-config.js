4d230b162ea877b5c5497f489eb601ce9dad3b34
Jérémy JOURDIN <jeremy.jourdin@free.fr>
2018-07-19 15:49:50 +0200

    Improve configuration (build path + babel config

---
+++ babel-config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	ignore: /\/(build|node_modules\/(?!react-ssr))/,
+	presets: ['env', 'react-app']
+}
--- src/index.js
+++ src/index.js
@@ -7,16 +7,22 @@ var pjson   = require('../package.json');
 
 var App;
 
-require('babel-register')({
-  ignore: /\/(build|node_modules\/(?!react-ssr))/,
-  presets: ['env', 'react-app']
-});
-
 program
   .version(pjson.version)
   .arguments('<file>')
   .option('-p, --port <port>', 'Specify the listen port', parseInt, process.env.PORT || 3000)
-  .action(function (file) {
+  .option('-b, --buildPath <path>', 'Specify the build path', process.env.BUILD_PATH || '/build')
+  .option('-c, --babelConfigPath <path>', 'Specify the babel config file path (absolute)', Path.resolve(__dirname, '..', 'babel-config.js'))
+  .action(function (file, cmd) {
+    process.env.BUILD_PATH = cmd.buildPath;
+    process.env.PORT = cmd.port;
+
+    var config = require(Path.resolve(process.cwd(), cmd.babelConfigPath));
+
+    console.log(config)
+
+    require('babel-register')(config);
+
     app = require(Path.resolve(process.cwd(), file)).default;
   })
   .parse(process.argv);
@@ -31,6 +37,7 @@ if (!app) {
 // Why don't I need http createServer
 app.listen(program.port, ()=>{
   console.log(`App listening on port ${program.port}!`);
+  app.emit('listening', program.port)
 });
 
 app.on('error', onError);
